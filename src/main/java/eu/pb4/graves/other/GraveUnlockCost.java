82f541cb8b4475006b9e393e587ad951587fd740
Samuel <64842010+R3Neer@users.noreply.github.com>
2026-08-31 18:37:40 +0200

    Separate grave access policy from unlock cost

--- src/main/java/eu/pb4/graves/other/GraveUnlockCost.java
+++ src/main/java/eu/pb4/graves/other/GraveUnlockCost.java
@@ -10,19 +10,20 @@ public interface GraveUnlockCost {
         return this.quote(grave, grave.isOwner(player));
     }
 
+    default GenericCost<?> baseQuote(Grave grave) {
+        return this.quote(grave, true);
+    }
+
     boolean requiresPayment();
-
-    default boolean unlocksPerPlayer() {
-        return false;
-    }
-
-    default boolean allowsNonOwnerPayment() {
-        return false;
-    }
 
     record Static(GenericCost<?> cost) implements GraveUnlockCost {
         @Override
         public GenericCost<?> quote(Grave grave, boolean owner) {
+            return this.cost;
+        }
+
+        @Override
+        public GenericCost<?> baseQuote(Grave grave) {
             return this.cost;
         }
 
